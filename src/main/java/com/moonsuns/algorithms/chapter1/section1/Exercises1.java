8bf8b70042c719843c5c00ec00e2ed76c0e34d98
MoonsunS <yueyangsong@foxmail.com>
2018-08-12 00:23:00 +0800

    Exercises1

---
+++ src/main/java/com/moonsuns/algorithms/chapter1/section1/Exercises1.java
@@ -0,0 +1,11 @@
+package com.moonsuns.algorithms.chapter1.section1;
+
+public class Exercises1
+{
+    public static void main(String[] args)
+    {
+        System.out.println((0 + 15) / 2);
+        System.out.println(2.0e-6 * 100000000.1);
+        System.out.println(true && false || true && true);
+    }
+}
